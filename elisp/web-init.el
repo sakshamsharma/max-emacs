e2bea4cc7b6eee2bef5b4a6b8227e250706a8510
Saksham Sharma <saksham0808@gmail.com>
2016-05-18 17:12:17 +0200

    Add scss mode

--- elisp/web-init.el
+++ elisp/web-init.el
@@ -11,5 +11,15 @@
 (use-package php-mode
   :ensure t)
 
+(use-package scss-mode
+  :ensure t
+  :config
+  (autoload 'scss-mode "scss-mode")
+  (setq scss-compile-at-save nil)
+  (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode)))
+
+(use-package helm-css-scss
+  :ensure t)
+
 (provide 'web-init)
 ;;; web-init.el ends here
