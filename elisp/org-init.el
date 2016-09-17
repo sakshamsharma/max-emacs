169230e1871b2dea354592aa2edb6228523c07aa
Saksham Sharma <saksham0808@gmail.com>
2016-09-17 19:36:55 +0530

    Add cdlatex and md export to org mode

--- elisp/org-init.el
+++ elisp/org-init.el
@@ -11,7 +11,9 @@
   (define-key global-map "\C-cl" 'org-store-link)
   (define-key global-map "\C-ca" 'org-agenda)
   (define-key global-map "\C-cg" 'org-show-subtree)
-  (setq org-log-done t))
+  (setq org-log-done t)
+  (eval-after-load "org"
+    '(require 'ox-md nil t)))
 
 (provide 'org-init)
 ;;; org-init.el ends here
