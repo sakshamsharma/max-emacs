70b10e310f74d88f336322c054431f0efb1bb8f8
Saksham Sharma <saksham0808@gmail.com>
2016-09-25 18:07:28 +0530

    Switch to elpy, solve all troubles

--- elisp/py-init.el
+++ elisp/py-init.el
@@ -5,20 +5,10 @@
 
 (require 'use-package)
 
-(use-package anaconda-mode
+(use-package elpy
   :ensure t
   :config
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-
-  (add-to-list 'python-shell-extra-pythonpaths "/home/saksham/code/movi/movi")
-  (use-package company-anaconda
-    :ensure t)
-
-  (use-package pyvenv
-    :ensure t
-    :config
-    (setenv "WORKON_PATH" "$HOME/.envs")))
+  (elpy-enable))
 
 (provide 'py-init)
 ;;; py-init.el ends here
