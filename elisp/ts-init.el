0832b4561c53efbc777882139ee37c6c56313c3e
Saksham Sharma <saksham0808@gmail.com>
2017-01-24 23:21:42 +0530

    Standardize my web lisp

--- elisp/ts-init.el
+++ elisp/ts-init.el
@@ -5,19 +5,6 @@
 
 (require 'use-package)
 
-;; (use-package typescript
-;;   :ensure t
-;;   :config
-;;   (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
-;;   (use-package tss
-;;     :ensure t
-;;     :config
-;;     (setq tss-popup-help-key "C-:")
-;;     (setq tss-jump-to-definition-key "C->")
-;;     (setq tss-implement-definition-key "C-c i")
-;;     (tss-config-default)
-;;     ))
-
 (use-package tide
   :ensure t
   :config
@@ -29,12 +16,9 @@
               (eldoc-mode +1)
               (company-mode-on)))
   ;; aligns annotation to the right hand side
-  (setq company-tooltip-align-annotations t)
   (setq typescript-indent-level 2)
   :bind (("M-." . tide-jump-to-definition-reuse-window)))
 
-(add-to-list 'load-path "/home/saksham/.emacs.d/emacs-gulpjs")
-(autoload 'gulpjs-start-task "gulpjs" "Start a gulp task." t)
 (setq typescript-indent-level 2)
 
 (provide 'ts-init)
--- elisp/web-init.el
+++ elisp/web-init.el
@@ -18,12 +18,12 @@
   (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
   (defun my-web-mode-hook ()
     "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 4))
+    (setq web-mode-markup-indent-offset 2))
   (add-hook 'web-mode-hook  'my-web-mode-hook)
-  (setq web-mode-markup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq web-mode-code-indent-offset 4)
-  (setq web-mode-js-indent-offset 4))
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-js-indent-offset 2))
 
 (use-package php-mode
   :ensure t)
