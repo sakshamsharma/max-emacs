a5ca304172c87f64416db7d00178269e919a565e
Saksham Sharma <saksham0808@gmail.com>
2018-04-01 15:09:12 +0530

    Better python editing.

--- elisp/py-init.el
+++ elisp/py-init.el
@@ -5,22 +5,24 @@
 
 (require 'use-package)
 
-;; (use-package elpy
-;;   :ensure t
-;;   :functions (elpy-enable)
-;;   :mode ("\\.py\\'" . python-mode)
-;;   :config
-;;   (elpy-enable))
-
-(use-package anaconda-mode
+(use-package elpy
   :ensure t
+  :functions (elpy-enable)
   :mode ("\\.py\\'" . python-mode)
   :config
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  (elpy-enable))
+  ;; :bind (:map python-mode-map (("M-." . elpy-goto-definition)
+  ;;                              ("M-,"))))
 
-  (use-package company-anaconda
-    :ensure t))
+;; (use-package anaconda-mode
+;;   :ensure t
+;;   :mode ("\\.py\\'" . python-mode)
+;;   :config
+;;   (add-hook 'python-mode-hook 'anaconda-mode)
+;;   (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+
+;;   (use-package company-anaconda
+;;     :ensure t))
 
 (provide 'py-init)
 ;;; py-init.el ends here
