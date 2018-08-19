0c2011354140b1a8e3e25e48be27c1f8bd8cda5f
Saksham Sharma <saksham0808@gmail.com>
2018-08-19 11:42:24 -0400

    Remove marmalade.

--- init.el
+++ init.el
@@ -12,8 +12,7 @@
  package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                     ("org" . "http://orgmode.org/elpa/")
                     ("melpa-stable" . "http://stable.melpa.org/packages/")
-                    ("melpa" . "http://melpa.org/packages/")
-                    ("marmalade" . "http://marmalade-repo.org/packages/")))
+                    ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
